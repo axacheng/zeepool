743027148f10e946f510f53c44830b6b8c3d8e5e
axa <axa@192.168.1.161>
2012-05-09 01:34:12 +0800

    tiny code clean up

--- src/weibo_oauth_v2.py
+++ src/weibo_oauth_v2.py
@@ -138,7 +138,6 @@ class LogoutHandler(BaseHandler):
             
 def set_cookie(response, name, value, domain=None, path="/", expires=None):
     """Generates and signs a cookie for the give name/value"""
-    import sys
     timestamp = str(int(time.time()))
     value = base64.b64encode(value)
     signature = cookie_signature(value, timestamp)
